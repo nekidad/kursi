c49e4c680a6ee27c633237b31b774e141d846d5a
nekidad <coldcooper@gmail.com>
2022-03-22 09:59:49 +0400

    first commit

---
+++ scripte.js
@@ -0,0 +1,77 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMerried: false
+// };
+// // Console.log(obj.name);
+// Console.log(obj.name);
+
+// let arr = ["plum.png", "blue.jpg", 6, "apple.bmp", []];
+// console.log(arr[4]);
+
+// Другая тема
+
+// "use sctrict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть 18 лет?", "18");
+// console.log(answer + 12);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+"use sctrict";
+
+console.log("word" + 'text');
+console.log('word' + " - are the world");
+console.log("4" + '6');
+console.log(4 + '6');
+console.log(4 + 6);
+
+let incr = 20,
+    decr = 20;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = true;
+console.log(isChecked && isClose);
+
